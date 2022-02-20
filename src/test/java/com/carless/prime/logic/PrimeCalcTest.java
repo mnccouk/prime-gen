de247e177fb8dbf75d071323419e192a3648d4ea
mnc <matt.carless@handroid.co.uk>
2022-02-20 19:34:03 +0000

    Implemented test for prime number generator.

--- src/test/java/com/carless/prime/logic/PrimeCalcTest.java
+++ src/test/java/com/carless/prime/logic/PrimeCalcTest.java
@@ -2,9 +2,16 @@ package com.carless.prime.logic;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 class PrimeCalcTest {
+	
+	Logger logger = LoggerFactory.getLogger(PrimeCalcTest.class);
 
 	@Test
 	void testIsPrime() {
@@ -16,8 +23,16 @@ class PrimeCalcTest {
 	@Test
 	void testNextPrime() {
 		IPrimes primeCalc = new PrimeCalc();
-		int nextPrime = primeCalc.nextPrime(1100);
-		System.out.println("Next Prime:" + nextPrime);
+		Random rn = new Random();
+		int nextPrime = 4;
+		for (int i = 0;i < 100;i++) {
+			int primeRndSeed = rn.nextInt(Integer.MAX_VALUE)+1;
+			logger.debug("Generated random number: {}", primeRndSeed);
+			nextPrime = primeCalc.nextPrime(primeRndSeed);
+			logger.debug("Generated prime number: {}", nextPrime);
+			assertTrue(primeCalc.isPrime(nextPrime),"Generated number did not pass prime number check, the number was:" + nextPrime);
+		}
+		
 	}
 
 }
