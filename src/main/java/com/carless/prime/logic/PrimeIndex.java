e6771990f1e13905a6dc72ac05f8ab94a2223143
mnc <matt.carless@handroid.co.uk>
2022-02-22 08:07:03 +0000

    Added debug code and documentation.

--- src/main/java/com/carless/prime/logic/PrimeIndex.java
+++ src/main/java/com/carless/prime/logic/PrimeIndex.java
@@ -6,12 +6,39 @@ import java.util.SortedMap;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+/**
+ * Class that loads a prime index file with a method to find the nearest next lowest
+ * closest match to an index value passed in to the findCloseIndexLocation. It
+ * is used to increase performance when paging prime data with high values.
+ * Using the index helps paging as we need to only process primes around the
+ * area we need to return data for.
+ * 
+ * The format of the index file is
+ *  
+ *  idx:prime
+ *  
+ *  for example 
+ *  
+ *  1:2  //represents the first entry in the index, 1 being the index value 2 being the prime number at that index.
+ *  1000000:15485863  // this is 1 millionth prime
+ *  ...
+ *  ...
+ *  ...
+ *  9000000:160481183 //9th millionth prime.
+ *  
+ * @author mnc
+ *
+ */
 @Component
 public class PrimeIndex {
+
+	Logger logger = LoggerFactory.getLogger(PrimeIndex.class);
 
 	private SortedMap<Integer, Integer> primeIndexMap = new TreeMap<>();
 
@@ -26,6 +53,7 @@ public class PrimeIndex {
 	 */
 	public SortedMap<Integer, Integer> loadIndex() {
 		if (primeIndexMap.isEmpty()) {
+			logger.debug("Loading Prime index from the {}", resourceFile.getFilename());
 			try (BufferedReader br = new BufferedReader(new FileReader(resourceFile.getFile()))) {
 				String line;
 				while ((line = br.readLine()) != null) {
@@ -34,9 +62,12 @@ public class PrimeIndex {
 					Integer prime = Integer.parseInt(st.nextToken()); // Prime at idx
 					primeIndexMap.put(index, prime);
 				}
+				logger.debug("Prime index file loaded.");
 			} catch (Exception e) {
 				e.printStackTrace();
-
+				logger.error("Error loading prime index file {}. The error was {} ", resourceFile.getFilename(),
+						e.getMessage());
+				;
 			}
 		}
 		return primeIndexMap;
@@ -52,13 +83,17 @@ public class PrimeIndex {
 	 */
 	public PrimeIndex.PrimeIdxData findCloseIndexLocation(Integer toFind) {
 		loadIndex();
+		logger.debug("Looking for nearest(next lowest) index number in index cache using value of: {}", toFind);
 		int previousKey = -1;
 		for (Integer key : primeIndexMap.keySet()) {
 			if (key > toFind) {
+				logger.debug("Found index match. Using index value: {} with prime {}", previousKey,
+						primeIndexMap.get(previousKey));
 				break;
 			}
 			previousKey = key;
 		}
+
 		return new PrimeIndex.PrimeIdxData(previousKey, primeIndexMap.get(previousKey));
 	}
 
